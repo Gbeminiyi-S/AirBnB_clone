99935f2250e160a6d623eb2777f116d48f1112ca
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-19 18:18:39 +0100

    reinitialising Basemodel instance from dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """This Module creates the class BaseModel"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if (key == 'created_at') or (key == 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """prints an instance of the object specified"""
