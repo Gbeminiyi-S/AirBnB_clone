f0a21c1869565945af32f420dcef8104b75a5fda
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-06-22 21:15:11 +0000

    Adding a console to the project

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module defines the class `HBHBCommand`"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the entry point of the command interpreter"""
+    intro = "Welcome to the AirBnB Console"
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exits the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """Exits the program
+        """
+        return True
+
+    def emptyline(self):
+        """Defines what happens when the `Enter` key is pressed
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
