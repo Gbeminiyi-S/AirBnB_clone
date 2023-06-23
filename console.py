2ab443ff0ca212f0834e8be5cc05a62144d3ba6d
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-23 15:21:31 +0100

    updated console with do_create

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module defines the class `HBHBCommand`"""
 import cmd
+from models.base_model import BaseModel
 
+classes = ["BaseModel"]
 
 class HBNBCommand(cmd.Cmd):
     """Defines the entry point of the command interpreter"""
@@ -21,6 +23,20 @@ class HBNBCommand(cmd.Cmd):
         """Defines what happens when the `Enter` key is pressed
         """
         pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id
+        """ 
+        args = args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            instance = eval(args[0])()
+            instance.save()
+            print(instance.id)
 
 
 if __name__ == '__main__':
