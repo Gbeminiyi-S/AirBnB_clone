a7898ac42986cf62904d34ffa984ab9a277ed50d
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-23 15:59:27 +0100

    updated console with do_destroy

--- console.py
+++ console.py
@@ -2,8 +2,10 @@
 """This module defines the class `HBHBCommand`"""
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 classes = ["BaseModel"]
+
 
 class HBNBCommand(cmd.Cmd):
     """Defines the entry point of the command interpreter"""
@@ -27,7 +29,7 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """Creates a new instance of BaseModel, saves it (to the JSON file)
         and prints the id
-        """ 
+        """
         args = args.split()
         if len(args) == 0:
             print("** class name missing **")
@@ -42,9 +44,6 @@ class HBNBCommand(cmd.Cmd):
         """Prints the string representation of an instance based on the
             class name and id
         """
-        from models import storage
-
-
         args = args.split()
         if len(args) == 0:
             print("** class name missing **")
@@ -58,6 +57,24 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 print(storage.all()[instance_key])
+
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id
+        """
+        args = args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            instance_key = f'{args[0]}.{args[1]}'
+            if instance_key not in storage.all():
+                print("** no instance found **")
+            else:
+                del(storage.all()[instance_key])
+                storage.save()
 
 
 if __name__ == '__main__':
