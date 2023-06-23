cc28d6a8bd1990584af393e4c4503ed0f74f2c16
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-23 15:37:36 +0100

    updated console with do_show

--- console.py
+++ console.py
@@ -38,6 +38,27 @@ class HBNBCommand(cmd.Cmd):
             instance.save()
             print(instance.id)
 
+    def do_show(self, args):
+        """Prints the string representation of an instance based on the
+            class name and id
+        """
+        from models import storage
+
+
+        args = args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            instance_key = f'{args[0]}.{args[1]}'
+            if instance_key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[instance_key])
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
