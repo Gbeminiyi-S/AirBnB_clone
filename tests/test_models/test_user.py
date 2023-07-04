e7edce92a1abe0e1333b06de3529d2765190794f
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-07-04 14:29:37 +0000

    adding unittests for user.email

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,7 +12,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_instance_attributes(self):
         user = User()
-        self.assertIsInstance(user.id, str)
+        self.assertIsInstance(user.email, str)
         self.assertIsInstance(user.password, str)
         self.assertIsInstance(user.first_name, str)
         self.assertIsInstance(user.last_name, str)
