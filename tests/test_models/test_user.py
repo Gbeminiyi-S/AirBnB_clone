48ce39d982b73b076144571a74db6e2698c62c63
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-07-04 14:32:43 +0000

    update to pycode styling

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,17 +5,15 @@ from models.user import User
 from models.base_model import BaseModel
 import os
 
-def is_string_empty(string):
-    return len(string) == 0
 
 class TestBaseModel(unittest.TestCase):
-
     def test_instance_attributes(self):
         user = User()
         self.assertIsInstance(user.email, str)
         self.assertIsInstance(user.password, str)
         self.assertIsInstance(user.first_name, str)
         self.assertIsInstance(user.last_name, str)
+
 
 if __name__ == '__main__':
     unittest.main()
